b18e37ea8023ffcd35a917afe2826c1a6be52165
valeriaanaf <155537342+valeriaanaf@users.noreply.github.com>
2026-07-21 19:56:17 +0700

    fix: add remotePattern untuk backend images

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,27 @@ const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "backsakanca-production.up.railway.app",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.sakanca.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "sakanca.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
 export default nextConfig;
- 
